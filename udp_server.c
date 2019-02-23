d288c832b13195ee037ba1959fc3225819eb0d36
Felicious <fkuan@scu.edu>
2019-02-22 17:10:31 -0800

    fix skip

--- udp_server.c
+++ udp_server.c
@@ -67,7 +67,7 @@ int main (int argc, char *argv[])
 		perror("Received file from client\n");
 
 		//five percent chance of ignoring files sent by client
-		if(rand() % 100 < 30){
+		if(rand() % 100 < 5){
 			perror("Received but ignored file from client muahahahahaha >:D\n");
 			//doesn't do anything because client should timeout
 			continue;
