88fbc8e498ecc2210784fd26b6601ad20d445a1c
Felicious <fkuan@scu.edu>
2019-03-01 15:45:47 -0800

    print

--- Lab 5/lab5.c
+++ Lab 5/lab5.c
@@ -6,12 +6,39 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
+#include <"helper_fxns.h">
 
 /***********
  *  main
  ***********/
 int main (int argc, char *argv[])
 {
+	perror("Top of the program!\n");
+
+/*
+	if (argc < 5)
+	{
+		printf ("parameters: %s <machine_id> <num_of_machines> <costs_file> <machines_file>\n");
+		return 1;
+	}
+*/
+	//parse the costs file 
+	FILE *cost;
+
+	costs = fopen(argv[3], "rb");
+	if(!cost){
+		printf("File cannot be opened\n");
+		return 0;
+	}
+
+	int matrix[16];
+
+	for(int i = 0; i < 16; i++)
+		fscanf(costs, "%d", matrix[i]);
+
+	pmatrix(matrix);
+
+	
 
 
 }
