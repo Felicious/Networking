e393a3793a623057bac8681924ffd695336af31e
Felicious <fkuan@scu.edu>
2019-03-08 16:32:18 -0800

    fix type

--- Lab 5/matrices.c
+++ Lab 5/matrices.c
@@ -4,7 +4,7 @@
 /*matrices.c*/
 
 /* prints the matrix */
-void pmatrix(int matrix[sz][sz], int sz)
+void pmatrix(int matrix[int sz][int sz], int sz)
 {
 	for(int row = 0; row < sz; row++)
 	{
@@ -13,7 +13,7 @@ void pmatrix(int matrix[sz][sz], int sz)
 }
 
 
-void init_matrix(int matrix[N][N], int N)
+void init_matrix(int matrix[int N][int N], int N)
 {
 	FILE *cost;
 
--- Lab 5/matrices.h
+++ Lab 5/matrices.h
@@ -1,7 +1,7 @@
 /* matrices.h */
 
 //function that prints the matrix
-void pmatrix(int matrix[sz][sz], int sz);
+void pmatrix(int matrix[int sz][int sz], int sz);
 
-void init_matrix(int matrix[N][N], int N);
+void init_matrix(int matrix[int N][int N], int N);
 
