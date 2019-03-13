94671a7e39ad812064e7e5e99fe6af800d30c5ff
Felicious <fkuan@scu.edu>
2019-03-13 15:07:37 -0700

    fix auto

--- Lab 5/automata.c
+++ Lab 5/automata.c
@@ -5,14 +5,14 @@ Automata automata[4];
 void automata_config(FILE *fpp){
     if(fpp == NULL){
 		printf("Problem opening Automata naming file!\n");
-		return 0;
+		return;
 	}
 
     for(int i = 0; i < host_no; i++){
         //copying data from host name file 
         //into automata data struct
     	fscanf(fpp, "%s\t%s\t%d\n", automata[i].name, 
-            automata[i].ip, &automata[i].port);
+            automata[i].ip_addr, &automata[i].port_no);
     }
 
     fclose(fpp);
