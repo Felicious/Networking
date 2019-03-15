36f47d0acff5ec54574aacad1330b482be42dd5f
Felicious <fkuan@scu.edu>
2019-03-15 14:49:35 -0700

    fix header

--- Lab 5/threads.h
+++ Lab 5/threads.h
@@ -39,7 +39,7 @@ of initialization procedures for est. connections
 void* A2(void* arg);
 
 //helper function for thread 2
-int Pod_042(int *map, Automata *machine);
+int Pod_042(int *map, int i);
 
 /*
 THREAD # 2
